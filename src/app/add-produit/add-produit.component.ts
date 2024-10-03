32c425096588da6fb7edbe8858541feef0b20bc3
Mohamedkhayati <mohamedkhayati261@gmail.com>
2024-10-03 19:49:47 +0100

    Add files via upload

--- src/app/add-produit/add-produit.component.ts
+++ src/app/add-produit/add-produit.component.ts
@@ -1,12 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from '../services/produit.service';
+import { Produit } from '../model/produit.model';
+
 
 @Component({
   selector: 'app-add-produit',
-  templateUrl: './add-produit.component.html'
+  templateUrl: './add-produit.component.html',
+  
 })
 export class AddProduitComponent implements OnInit {
-  constructor(){}
+  [x: string]: any;
+  newProduit = new Produit();
+  message=String ;
+
+  constructor(private produitService: ProduitService){}
   ngOnInit(): void {
     
   }
+  addProduit(){
+    console.log(this.newProduit);
+    this.produitService.ajouterProduit(this.newProduit);
+    
+    }
 }
--- src/app/produits/produits.component.ts
+++ src/app/produits/produits.component.ts
@@ -1,15 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ProduitService } from '../services/produit.service';
+import { Produit } from '../model/produit.model';
 
 @Component({
   selector: 'app-produits',
-  templateUrl: './produits.component.html'
+  templateUrl: './produits.component.html',
+  
 })
 export class ProduitsComponent implements OnInit {
-  produits : string[]; //un tableau de chînes de caractères
-  constructor(){
-    this.produits = ["PC Asus", "Imprimante Epson", "Tablette Samsung"];
+  produits : Produit[];
+  constructor(private produitService: ProduitService){
+    this.produits = produitService.listeProduits();
+
+    
   }
   ngOnInit(): void {
     
   }
+  supprimerProduit(p: Produit)
+  {
+  //console.log(p);
+ let conf = confirm("Etes-vous sûr ?");
+ if (conf)
+this.produitService.supprimerProduit(p);
+  }
+  
 }
